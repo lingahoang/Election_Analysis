c0e44a830ddc9a2a4463102a688410e86d4a3ee4
Ling Hoang <linghoang@Lings-MacBook-Air.local>
2022-07-08 21:49:31 -0700

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,43 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
+
+# Add our dependencies.
+import csv
+import os
+
+# Assign a variable to load a file from the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Open the election result and read the file.
+with open(file_to_load) as election_data:
+# To do: read and analyze the data here
+# Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+# Print the header row.
+    headers = next(file_reader)
+        print(headers)
+
+   
+# Close the file.
+election_data.close()
+
+# Create a filename variable to a direct or indirect to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Counties in the Election")
+
+    # Write three counties to the file.
+    txt_file.write("Arapahoe, ")
+    txt_file.write("Denver, ")
+    txt_file.write("Jefferson")
+
+# Close the file.
+outfile.close()
